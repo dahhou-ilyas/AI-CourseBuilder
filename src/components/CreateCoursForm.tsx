477a631896995cd44465d9598f239f7764b452da
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-25 20:39:49 +0100

    finish the form

--- src/components/CreateCoursForm.tsx
+++ src/components/CreateCoursForm.tsx
@@ -7,6 +7,8 @@ import { createChaptersSchema } from '@/app/validation/cours'
 import { useForm } from 'react-hook-form'
 import {zodResolver} from "@hookform/resolvers/zod"
 import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { Plus, PlusCircle, Trash2 } from 'lucide-react'
 
 type Props = {}
 
@@ -30,14 +32,14 @@ function CreateCoursForm({}: Props) {
     <div className='w-full'>
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className='w-full mt-4 '>
-                <FormField control={form.control} name='title'
+                <FormField control={form.control} name='title' 
                 render={({field})=>{
-                    return (<FormItem className='flex flex-col items-start w-full sm:items-center sm:flex-row'>
-                        <FormLabel className='flex-1 text-xl'>
+                    return (<FormItem className='w-full flex flex-col justify-center items-center'>
+                        <FormLabel className='text-xl'>
                             Title
                         </FormLabel>
-                        <FormControl className='flex-[6]'>
-                            <Input placeholder='Enter the main topic of the cours' className='bg-indigo-300 border-none dark:bg-black' {...field}/>
+                        <FormControl className=''>
+                            <Input placeholder='Enter subtopic of the course' className='bg-indigo-300 border-none dark:bg-black' {...field}/>
                         </FormControl>
                     </FormItem>)
                 }}
@@ -49,11 +51,11 @@ function CreateCoursForm({}: Props) {
                     <FormField control={form.control} name={`units.${index}`}
                         render={({field})=>{
                             return (
-                                <FormItem className='flex flex-col items-start w-full sm:items-center sm:flex-row'>
-                                    <FormLabel className='flex-1 text-xl'>
+                                <FormItem className='w-full flex mt-3 flex-col justify-center items-center'>
+                                    <FormLabel className='text-xl'>
                                         Unit {index+1}
                                     </FormLabel>
-                                    <FormControl className='flex-[6]'>
+                                    <FormControl className=''>
                                         <Input placeholder='Enter the main topic of the cours' className='bg-indigo-300 border-none dark:bg-black' {...field}/>
                                     </FormControl>
                                 </FormItem>
@@ -62,6 +64,12 @@ function CreateCoursForm({}: Props) {
                     />
                 )
             })}
+
+
+            <div className='flex items-center justify-center mt-6 gap-x-5 '>
+                <Button onClick={()=>form.setValue('units',[...form.watch('units'),''])} type='button' variant={"default"} className='font-semibold'>Add Unit <PlusCircle className='ml-2 text-green-700'/></Button>
+                <Button onClick={()=>form.setValue('units',form.watch('units').slice(0, -1))} type='button' variant={"default"} className='font-semibold'>Remove Unit <Trash2 className='ml-2 text-red-700'/></Button>
+            </div>
         </Form>
     </div>
   )
