f282e72e0d99bc199571df7283f13c55e90377d8
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-26 19:21:43 +0100

    change

--- src/components/CreateCoursForm.tsx
+++ src/components/CreateCoursForm.tsx
@@ -46,38 +46,36 @@ function CreateCoursForm({}: Props) {
                     </FormItem>)
                 }}
                 />
-
+                    <AnimatePresence>
+                    {form.watch("units").map((_,index)=>{
+                        return (
+                            <motion.div key={index} initial={{opacity:0,height:0}} animate={{opacity:1,height:"auto"}} exit={{opacity:0,height:0}} transition={{opacity:{duration:0.2},height:{duration:0.2}}}>
+                                <FormField control={form.control} name={`units.${index}`}
+                                render={({field})=>{
+                                    return (
+                                        <FormItem className='w-full flex mt-3 flex-col justify-center items-center'>
+                                            <FormLabel className='text-xl font-mono'>
+                                                Unit {index+1}
+                                            </FormLabel>
+                                            <FormControl className=''>
+                                                <Input placeholder='Enter the main topic of the cours' className='bg-indigo-300 border-none dark:bg-black' {...field}/>
+                                            </FormControl>
+                                        </FormItem>
+                                    )
+                                }}
+                                />
+                            </motion.div>
+                        )
+                    })}
+                </AnimatePresence>
+                <div className='flex items-center justify-center mt-6 gap-x-5 '>
+                    <Button onClick={()=>form.setValue('units',[...form.watch('units'),''])} type='button' variant={"default"} className='font-semibold'>Add Unit <PlusCircle className='ml-2 text-green-700'/></Button>
+                    <Button onClick={()=>form.setValue('units',form.watch('units').slice(0, -1))} type='button' variant={"default"} className='font-semibold'>Remove Unit <Trash2 className='ml-2 text-red-700'/></Button>
+                </div>
+                <div className='w-full flex items-center justify-center'>
+                    <Button type='submit' size={"lg"} className='font-mono w-[50%] mt-5'>Generating</Button> 
+                </div>   
             </form>
-
-            <AnimatePresence>
-                {form.watch("units").map((_,index)=>{
-                    return (
-                        <motion.div key={index} initial={{opacity:0,height:0}} animate={{opacity:1,height:"auto"}} exit={{opacity:0,height:0}} transition={{opacity:{duration:0.2},height:{duration:0.2}}}>
-                            <FormField control={form.control} name={`units.${index}`}
-                            render={({field})=>{
-                                return (
-                                    <FormItem className='w-full flex mt-3 flex-col justify-center items-center'>
-                                        <FormLabel className='text-xl font-mono'>
-                                            Unit {index+1}
-                                        </FormLabel>
-                                        <FormControl className=''>
-                                            <Input placeholder='Enter the main topic of the cours' className='bg-indigo-300 border-none dark:bg-black' {...field}/>
-                                        </FormControl>
-                                    </FormItem>
-                                )
-                            }}
-                            />
-                        </motion.div>
-                    )
-                })}
-            </AnimatePresence>
-            
-
-
-            <div className='flex items-center justify-center mt-6 gap-x-5 '>
-                <Button onClick={()=>form.setValue('units',[...form.watch('units'),''])} type='button' variant={"default"} className='font-semibold'>Add Unit <PlusCircle className='ml-2 text-green-700'/></Button>
-                <Button onClick={()=>form.setValue('units',form.watch('units').slice(0, -1))} type='button' variant={"default"} className='font-semibold'>Remove Unit <Trash2 className='ml-2 text-red-700'/></Button>
-            </div>
         </Form>
     </div>
   )
