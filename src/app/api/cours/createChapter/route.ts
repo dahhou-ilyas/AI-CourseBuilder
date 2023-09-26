d4c570bc9363cddba569ae430d2f5b0bc8484318
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-26 22:19:34 +0100

    modifier le prompot

--- src/app/api/cours/createChapter/route.ts
+++ src/app/api/cours/createChapter/route.ts
@@ -14,22 +14,22 @@ export async function POST(req:Request,res:Response) {
             title:string;
             chapters:{
                 youtube_search_query:string;
-                chapter_titlz:string;
+                chapter_title:string;
             }[]
         }
         
 
         let output_unit:outputUnits=await strict_output(
             'You are an AI capable of curating course content, coming up with relevant chapter titles, and finding relevant youtube video for each chapter',
-            new Array(units.length).fill(
-                `It your job to create a course about ${title}. The user has requested to create chapters for each of the units. Then, for each chapter, provide a detailed youtube search query that can be used to find an informative educational video for each chapter. Each query should give an educational informative course in youtube.`
-            ),
+            units.map((elem)=>{
+                return `It your job to create a course about ${title}.First give him introduction of title, then the user has requested to create chapters for each of the units ${elem}. Then, for each chapter, provide a detailed youtube search query that can be used to find an informative educational video for each chapter. Each query should give an educational informative course in youtube.`
+            }),
             {
                 title:"title of the unit",
                 chapters:'an array of chapters, each chapter should have a youtube_search_query and a chapter_title key in the JSON object'
             }
         )
-        console.log(title);
+        
 
         console.log(output_unit);
 
