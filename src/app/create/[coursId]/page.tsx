7ed2e3ac44b081c9767cb59dbd69cb78f9049fb3
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-10-03 20:36:51 +0100

    create specifique page for cours id

--- src/app/create/[coursId]/page.tsx
+++ src/app/create/[coursId]/page.tsx
@@ -1,10 +1,41 @@
+import { getAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/db';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params:{
+    coursId:string;
+  }
+}
 
-function CreateChapters({}: Props) {
+async function CreateChapters({params:{coursId}}: Props) {
+  const session=await getAuthSession();
+  if(!session?.user){
+    return redirect('/gallery')
+  }
+
+  //la methode pour fair join
+  const cours=await prisma.course.findUnique({
+    where:{
+      id:coursId
+    },
+    include:{
+      units:{
+        include:{
+          chapters:true
+        }
+      }
+    }
+  })
+  if(!cours){
+    return redirect('/create')
+  }
+
   return (
-    <div>page</div>
+    <div className='flex flex-col items-start max-w-xl mx-auto my-16'> 
+    
+    </div>
   )
 }
 
